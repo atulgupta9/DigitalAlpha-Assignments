296666de02cef51254f63fcde2fb8153ebf864c6
atulgupta9 <atulgupta9@gmail.com>
2018-04-02 15:51:20 +0530

    Add files via upload

---
+++ Day1/listTest.py
@@ -0,0 +1,7 @@
+list= []
+print("Enter element for list")
+list.append(input())
+list.append(1)
+list.append("hi")
+list[1]= str(list[1])+"ust"
+print(list)
---
+++ Day1/strconcat.py
@@ -0,0 +1,6 @@
+print("Enter first string")
+string1 = str(input())
+print("Enter second string")
+string2 = str(input())
+concatenatedString = string1 + string2
+print(concatenatedString)
