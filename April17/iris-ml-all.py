fd513dd1a8541de81e25ca287ee11a659e073a4d
atulgupta9 <atulgupta9@gmail.com>
2018-04-17 17:24:06 +0530

    Add files via upload

--- April17/iris-ml-all.py
+++ April17/iris-ml-all.py
@@ -26,4 +26,56 @@ for i in range(len(y_test)):
     
     
 #=======================LOGISTIC REGRESSION================================#
-    
+
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import train_test_split
+import pandas as pd
+import numpy as np
+
+iris_dataset = datasets.load_iris()
+
+x = pd.DataFrame(iris_dataset.data)
+y = pd.DataFrame(iris_dataset.target)
+x.columns = iris_dataset.feature_names
+
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25)
+logistic_regression = LogisticRegression()
+logistic_regression.fit(x_train,y_train)
+
+print("Accuracy of Logistic Regression",logistic_regression.score(x_test,y_test))
+
+y_predict = logistic_regression.predict(x_test)
+
+print("Original \tvs\tPrediction")
+for i in range(len(y_test)):
+    print("%f \tvs\t%f"%(np.array(y_test)[i],y_predict[i]))
+
+
+#=======================SVM (Support Vector Machine)================================#  
+
+from sklearn import datasets
+from sklearn.svm import SVC
+from sklearn.cross_validation import train_test_split
+import pandas as pd
+import numpy as np
+
+iris_dataset = datasets.load_iris()
+x = pd.DataFrame(iris_dataset.data)
+y = pd.DataFrame(iris_dataset.target)
+
+x.columns = iris_dataset.feature_names
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.35)
+svm = SVC()
+svm.fit(x_train,y_train)
+
+print("Accuracy of SVM ",svm.score(x_test,y_test))
+
+y_predict = svm.predict(x_test)
+
+print("Original \tvs\tPrediction")
+for i in range(len(y_test)):
+    print("%f \tvs\t%f"%(np.array(y_test)[i],y_predict[i]))
+
