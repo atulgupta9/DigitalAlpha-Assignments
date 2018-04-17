ddae53c864a85da466a5fb9bea620766635bbd99
atulgupta9 <atulgupta9@gmail.com>
2018-04-17 16:48:57 +0530

    Add files via upload

---
+++ April17/iris-ml-all.py
@@ -0,0 +1,29 @@
+#=======================LINEAR REGRESSION================================#
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
+
+iris_dataset = datasets.load_iris()
+x= pd.DataFrame(iris_dataset.data)
+y = pd.DataFrame(iris_dataset.target)
+x.columns = iris_dataset.feature_names
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25)
+linear_regression = LinearRegression()
+linear_regression.fit(x_train,y_train)
+print("Label = %f %s + %f %s + %f %s + %f %s + %f"%(linear_regression.coef_[0][0],x.columns[0],linear_regression.coef_[0][1],x.columns[1],linear_regression.coef_[0][2],x.columns[2],linear_regression.coef_[0][3],x.columns[3],linear_regression.intercept_[0]))
+
+print("Accuracy of Linear Regression",linear_regression.score(x_test,y_test))
+
+y_predict = linear_regression.predict(x_test)
+
+print("Original \tvs\tPrediction")
+for i in range(len(y_test)):
+    print("%f \tvs\t%f"%(np.array(y_test)[i],y_predict[i]))
+    
+    
+#=======================LOGISTIC REGRESSION================================#
+    
