82df0d366e0d9e3de6828ea5a5200c2362d7e2dd
atulgupta9 <atulgupta9@gmail.com>
2018-04-17 17:55:56 +0530

    Add files via upload

--- April17/iris-ml-all.py
+++ April17/iris-ml-all.py
@@ -67,7 +67,7 @@ y = pd.DataFrame(iris_dataset.target)
 
 x.columns = iris_dataset.feature_names
 
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.35)
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25)
 svm = SVC()
 svm.fit(x_train,y_train)
 
@@ -79,3 +79,57 @@ print("Original \tvs\tPrediction")
 for i in range(len(y_test)):
     print("%f \tvs\t%f"%(np.array(y_test)[i],y_predict[i]))
 
+#=======================Naive Bayes ================================#  
+
+from sklearn import datasets
+from sklearn.naive_bayes import GaussianNB
+from sklearn.cross_validation import train_test_split
+
+import pandas as pd
+import numpy as np
+
+iris_dataset = datasets.load_iris()
+x = pd.DataFrame(iris_dataset.data)
+y = pd.DataFrame(iris_dataset.target)
+
+x.columns = iris_dataset.feature_names
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25,random_state=8)
+
+nb = GaussianNB()
+nb.fit(x_train,y_train)
+
+print("Accuracy of Naive Bayes ",nb.score(x_test,y_test))
+
+y_predict = nb.predict(x_test)
+
+print("Original \tvs\tPrediction")
+for i in range(len(y_test)):
+    print("%f \tvs\t%f"%(np.array(y_test)[i],y_predict[i]))
+
+#=======================K-Means ================================#  
+
+from sklearn import datasets
+from sklearn.cluster import KMeans
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
+
+import pandas as pd
+import numpy as np 
+
+iris_dataset = datasets.load_iris()
+x = pd.DataFrame(iris_dataset.data)
+y = pd.DataFrame(iris_dataset.target)
+
+x.columns = iris_dataset.feature_names
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25,random_state=8)
+
+kmean = KMeans(n_clusters=3)
+kmean.fit(x_train)
+
+print("Accuracy of Naive Bayes ",accuracy_score(y_test,kmean.predict(x_test)))
+
+y_predict = kmean.predict(x_test)
+
+print("Original \tvs\tPrediction")
+for i in range(len(y_test)):
+    print("%f \tvs\t%f"%(np.array(y_test)[i],y_predict[i]))
